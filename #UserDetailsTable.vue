f4d74f86fdec4034413b6c2d8aad86bd72a98d8a
Saichethan <117374159+bhukyasaichethan7@users.noreply.github.com>
2023-11-17 22:29:44 +0530

    Create #UserDetailsTable.vue

---
+++ #UserDetailsTable.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <!-- Table displaying user information -->
+    <!-- Implement search functionality here -->
+    <!-- Click action to open UserDetailsModal -->
+  </div>
+</template>
+
+<script>
+export default {
+  // Fetch user data from the placeholder database
+  data() {
+    return {
+      users: [], // User data
+    };
+  },
+  methods: {
+    // Function to open UserDetailsModal
+  },
+};
+</script>
+
+<style scoped>
+/* Table styling */
+</style>
