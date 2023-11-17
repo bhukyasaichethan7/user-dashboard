5e00dcfc88775938be00486a3cdb0ce30cdbe005
Saichethan <117374159+bhukyasaichethan7@users.noreply.github.com>
2023-11-17 22:30:57 +0530

    Create UserDetailsModel.vue

---
+++ UserDetailsModel.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <!-- Modal displaying detailed user information -->
+    <!-- Button to generate a report for the selected user -->
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    user: Object, // Selected user information
+  },
+  methods: {
+    // Function to generate a report for the selected user
+  },
+};
+</script>
+
+<style scoped>
+/* Modal styling */
+</style>
