d35f1c4e4acdb1dcc67f7ac135c7c7efd28391a8
Saichethan <117374159+bhukyasaichethan7@users.noreply.github.com>
2023-11-17 22:43:37 +0530

    Update UserDetailsModel.vue

--- UserDetailsModel.vue
+++ UserDetailsModel.vue
@@ -1,21 +1,11 @@
-<template>
-  <div>
-    <!-- Modal displaying detailed user information -->
-    <!-- Button to generate a report for the selected user -->
-  </div>
-</template>
+<// index.js or App.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Dashboard from './Dashboard';
 
-<script>
-export default {
-  props: {
-    user: Object, // Selected user information
-  },
-  methods: {
-    // Function to generate a report for the selected user
-  },
-};
-</script>
-
-<style scoped>
-/* Modal styling */
-</style>
+ReactDOM.render(
+  <React.StrictMode>
+    <Dashboard />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
