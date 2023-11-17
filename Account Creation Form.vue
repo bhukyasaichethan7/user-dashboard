34b027eb10d57e8f5de63cdd37edc40138a927a5
Saichethan <117374159+bhukyasaichethan7@users.noreply.github.com>
2023-11-17 22:32:13 +0530

    Create Account Creation Form.vue

---
+++ Account Creation Form.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <!-- Form for username and password input -->
+    <!-- Dummy request handling on form submission -->
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+    };
+  },
+  methods: {
+    // Dummy request handling function
+  },
+};
+</script>
+
+<style scoped>
+/* Form styling */
+</style>
