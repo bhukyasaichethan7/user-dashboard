85d38aee842eb968a14059835f30186f5207fbe6
Saichethan <117374159+bhukyasaichethan7@users.noreply.github.com>
2023-11-17 22:39:51 +0530

    Update #UserDetailsTable.vue

--- #UserDetailsTable.vue
+++ #UserDetailsTable.vue
@@ -1,25 +1,50 @@
-<template>
-  <div>
-    <!-- Table displaying user information -->
-    <!-- Implement search functionality here -->
-    <!-- Click action to open UserDetailsModal -->
-  </div>
-</template>
+// UserTable.js
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-<script>
-export default {
-  // Fetch user data from the placeholder database
-  data() {
-    return {
-      users: [], // User data
-    };
-  },
-  methods: {
-    // Function to open UserDetailsModal
-  },
+const UserTable = () => {
+  const [users, setUsers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    // Fetch user data from your API endpoint
+    axios.get('your-api-endpoint/users')
+      .then(response => setUsers(response.data))
+      .catch(error => console.error('Error fetching user data:', error));
+  }, []);
+
+  const filteredUsers = users.filter(user =>
+    user.username.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search by username"
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <table>
+        <thead>
+          <tr>
+            <th>Username</th>
+            <th>Email</th>
+            {/* Add more columns as needed */}
+          </tr>
+        </thead>
+        <tbody>
+          {filteredUsers.map(user => (
+            <tr key={user.id}>
+              <td>{user.username}</td>
+              <td>{user.email}</td>
+              {/* Add more cells as needed */}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
-</script>
 
-<style scoped>
-/* Table styling */
-</style>
+export default UserTable;
+
