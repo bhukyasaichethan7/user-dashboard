67e1c43c569474cf03ae9efba1cd718eb581e261
Saichethan <117374159+bhukyasaichethan7@users.noreply.github.com>
2023-11-17 22:41:01 +0530

    Update Account Creation Form.vue

--- Account Creation Form.vue
+++ Account Creation Form.vue
@@ -1,24 +1,41 @@
-<template>
-  <div>
-    <!-- Form for username and password input -->
-    <!-- Dummy request handling on form submission -->
-  </div>
-</template>
+// AccountCreationForm.js
+import React, { useState } from 'react';
+import axios from 'axios';
 
-<script>
-export default {
-  data() {
-    return {
-      username: '',
-      password: '',
-    };
-  },
-  methods: {
-    // Dummy request handling function
-  },
+const AccountCreationForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleCreateAccount = () => {
+    // Send the username and password to your API endpoint for account creation
+    axios.post('your-api-endpoint/create-account', { username, password })
+      .then(response => {
+        console.log('Account created successfully:', response.data);
+        // Optionally, you can reset the form after successful creation
+        setUsername('');
+        setPassword('');
+      })
+      .catch(error => console.error('Error creating account:', error));
+  };
+
+  return (
+    <div>
+      <label>Username:</label>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <label>Password:</label>
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleCreateAccount}>Create Account</button>
+    </div>
+  );
 };
-</script>
 
-<style scoped>
-/* Form styling */
-</style>
+export default AccountCreationForm;
+
